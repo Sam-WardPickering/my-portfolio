92af8f010640213108bcb711297b551a8e00ddc2
Sam Ward-Pickering <samuel.wardpickering@gmail.com>
2022-03-10 12:16:46 +1300

    Styling

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,7 +3,10 @@ import React from "react";
 function Header () {
     return(
         <div className="header-container">
-            <h1>Sam Ward-Pickering</h1>
+            <h1 className="header-name">Sam Ward-Pickering</h1>
+            <div className="link-icons">
+                <img className="github-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1024px-Octicons-mark-github.svg.png" alt="linkedin icon"/>
+            </div>
             
         </div>
     )
