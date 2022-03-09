7de5d9e7fbbf351010b8e694229d1e5f725e01b7
Sam Ward-Pickering <samuel.wardpickering@gmail.com>
2022-03-09 18:53:32 +1300

    Init styling set up

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header () {
     return(
-        <div>
+        <div className="header-container">
             <h1>Sam Ward-Pickering</h1>
             
         </div>
